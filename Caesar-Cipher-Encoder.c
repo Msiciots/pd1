996accd6e2fa9caa81310d032b9e5056a20368e9
Msiciots <f74049020@gs.ncku.edu.tw>
2020-11-21 03:32:32 +0800

    Make code clean

--- Caesar-Cipher-Encoder.c
+++ Caesar-Cipher-Encoder.c
@@ -2,39 +2,26 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int shift,reverse=0;
+    int shift;
     scanf("%d\n",&shift);
-    if(shift<0){
-        shift *= -1; 
-        reverse = 1;
-    }
     shift %= 26;
     char line[1000000];
-    scanf("%[^\n]", line);
+     scanf("%[^\n]", line);
+    /*gets(line);*/
     for(int i =0;i<strlen(line);i++){
         if('A'<=line[i] && line[i]<='Z'){
-            if(reverse){
-                line[i] -= shift;
-                if(line[i]<'A')
-                    line[i] += 26;
-            }
-            else{
             line[i] += shift;
             if(line[i]>'Z')
                 line[i] -= 26;
-            }
+            else if(line[i]<'A')
+                line[i] += 26;
         }
         else if ('a'<=line[i] && line[i]<='z'){
-            if(reverse){
-                line[i] -= shift;
-                if(line[i]<'a')
-                    line[i] += 26;
-            }
-            else{
             line[i] += shift;
             if(line[i]>'z')
                 line[i] -= 26;
-            }
+            else if(line[i]<'a')
+                line[i] += 26;
         }
     }
     printf("%s",line);
